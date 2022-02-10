a8a0fff6fbd404894c109e7519ba305b55f76b5c
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-11 01:25:31 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,11 +1,14 @@
 from telegram.ext import Updater
 import logging, os, time
+import telegram.ext as tg
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
 TOKEN = os.environ.get("TOKEN", None)
 
-updater = Updater(token='TOKEN')
+updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
+
+dispatcher = updater.dispatcher
 
 updater.start_polling()
