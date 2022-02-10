6e8cbedac243d36abdead707f9524f5eff8aefff
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-11 00:40:18 +0530

    Create main.py

---
+++ bot/main.py
@@ -0,0 +1,10 @@
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import CommandHandler
+from bot import TOKEN
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
