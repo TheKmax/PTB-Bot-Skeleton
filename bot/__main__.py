443a0dd47f5547226b6b8ba91833403c4b3c8a20
• BHARATHI • <86936565+Bharathi2003@users.noreply.github.com>
2022-02-11 01:31:12 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
-from bot import TOKEN
+from bot import TOKEN, dispatcher
 
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
